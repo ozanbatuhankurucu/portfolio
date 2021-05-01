14b8bec86c81f510ba52ae9ff6c58a2f684646d9
ozan batuhan kurucu <ozanbatusss@gmail.com>
2021-05-01 17:59:56 +0300

    gatsby build alındı

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -61,7 +61,7 @@ const Jobs = () => {
           })}
         </article>
       </div>
-      <Link to="/about" className="btn center-btn">
+      <Link to="/" className="btn center-btn">
         more info
       </Link>
     </section>
