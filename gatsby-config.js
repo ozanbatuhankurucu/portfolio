d49b3ac1e1f3e1baf748416e089a593b35572e7d
ozanbatuhankurucu <50013838+ozanbatuhankurucu@users.noreply.github.com>
2021-06-19 17:00:04 +0300

    test

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,7 +44,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: process.env.GA_TRACKING_ID,
+        trackingId: "G-XQ345MT4PG",
       },
     },
     // {
