cc4a426846c8d70ea02b1acc32590d950ce66355
ozanbatuhankurucu <50013838+ozanbatuhankurucu@users.noreply.github.com>
2021-12-20 14:52:35 +0300

    Fix Blog card height UI problem.

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -6,7 +6,7 @@ const Blog = ({ id, title, image, date, category, slug, desc }) => {
   return (
     <Link to={`/blogs/${slug}`} className="blog" key={id}>
       <article>
-        <Image fluid={image.childImageSharp.fluid} className="blog-img" />
+        <Image fluid={image?.childImageSharp?.fluid} className="blog-img" />
         <div className="blog-card">
           <h4>{title}</h4>
           <p>{desc}</p>
