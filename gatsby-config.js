c73aea48f33f04cd8138466669209a00c2da8874
ozanbatuhankurucu <50013838+ozanbatuhankurucu@users.noreply.github.com>
2022-01-16 17:23:33 +0300

    Test commit 2.

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,7 +36,7 @@ module.exports = {
         queryLimit: 1000, // Default to 100
         //   contentTypes : `jobs`, `projects`, `blogs`,
         //   singleType : `about`
-        //  ONLY ADD TO ARRAY IF YOU HAVE DATA IN STRAPI !!
+        //  ONLY ADD TO ARRAY IF YOU HAVE DATA IN STRAPI !
         contentTypes: [`jobs`, `projects`, `blogs`],
         singleTypes: [`about`],
       },
